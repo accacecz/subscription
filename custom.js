6dce1a0dbe6470367311f3cc5c83f28446199693
accacecz <125974570+accacecz@users.noreply.github.com>
2023-02-22 20:01:26 +0100

    Add files via upload

--- custom.js
+++ custom.js
@@ -7,8 +7,11 @@ location.search.slice(1).split("&").forEach(function(pair) {
 
 let id = params.id
 let response = params.response
+let lang = params.lang
 
-fetch(`https://prod-110.westeurope.logic.azure.com/workflows/e1566d2a1e144885a906ceece6889baa/triggers/manual/paths/invoke/ID/${id}/approval/${response}?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=lEWJl3LhpWYhLnQHNebtS_5m6MIFzr-VD43CU9QFSVk`)
+console.log(id, response, lang)
+
+//fetch(`https://prod-110.westeurope.logic.azure.com/workflows/e1566d2a1e144885a906ceece6889baa/triggers/manual/paths/invoke/ID/${id}/approval/${response}?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=lEWJl3LhpWYhLnQHNebtS_5m6MIFzr-VD43CU9QFSVk`)
 
 var countdownFrom = 5;  // number of seconds
 var countdownwin;
@@ -20,17 +23,28 @@ function CountDownStart() {
 
 function CountDownTimer(id)
 {
-    if (countdownFrom==0) {clearInterval(stp); window.close(); var cntText = "Pokud je toto okno stále otevřené, můžete ho zavřít."; x = document.getElementById(id); x.innerHTML = cntText;}
+    if (countdownFrom==0) {clearInterval(stp); window.close(); if(lang=="cs") {var cntText = "Pokud je toto okno stále otevřené, můžete ho zavřít.";} else if (lang=="en") {var cntText = "If this window is still open, you may close it manually.";}; x = document.getElementById(id); x.innerHTML = cntText;}
     else     {
                 var x
+                let thankyou = document.querySelector(".thankyou")
 
-                if (countdownFrom==5) {
-                    var cntText = "Toto okno se automaticky zavře za "+countdownFrom+" sekund.";
-                  } else if (countdownFrom==1) {
-                    var cntText = "Toto okno se automaticky zavře za "+countdownFrom+" sekundu.";
+                if(lang=="cs") {
+                  thankyou.innerHTML = "Děkujeme! Vaši odpověď jsme zaznamenali."
+                  if (countdownFrom==5) {
+                      var cntText = "Toto okno se automaticky zavře za "+countdownFrom+" sekund.";
+                    } else if (countdownFrom==1) {
+                      var cntText = "Toto okno se automaticky zavře za "+countdownFrom+" sekundu.";
+                    } else {
+                      var cntText = "Toto okno se automaticky zavře za "+countdownFrom+" sekundy.";
+                    }
+                } else if (lang=="en") {
+                  thankyou.innerHTML = "Thank you! Your response was submitted."
+                  if (countdownFrom==1) {
+                    var cntText = "This window will automatically close in "+countdownFrom+" second.";
                   } else {
-                    var cntText = "Toto okno se automaticky zavře za "+countdownFrom+" sekundy.";
+                    var cntText = "This window will automatically close in "+countdownFrom+" seconds.";
                   }
+                }
 
             if (document.getElementById)
         {
